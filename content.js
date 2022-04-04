91cb4260596636ae97ba0a317aee06156db43bd7
Guillaume <guignome01@gmail.com>
2022-04-04 18:49:29 -0400

    update design

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ if (typeof browser === "undefined") {
 
 var design = undefined
 var token = undefined;
-const version = 2.2
+const version = 1
 
 function notif(notification) {
 	let elem = document.getElementsByTagName("h1")[0]
